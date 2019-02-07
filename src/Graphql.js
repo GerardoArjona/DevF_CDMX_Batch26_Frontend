4f0e748799a1736f37224d076f95ec2ee46ab484
Gerardo Arjona <gerardo.arj15@gmail.com>
2019-02-06 20:01:18 -0600

    Add Dependecias necesarias para el proyecto
    Signed-off-by: Gerardo Arjona <gerardo.arj15@gmail.com>

---
+++ src/Graphql.js
@@ -0,0 +1,27 @@
+import { ApolloClient } from 'apollo-client' //Cliente de Grapql de Apollo
+import { createHttpLink } from 'apollo-link-http' //Trae el Schema de Graphql
+import { setContext } from 'apollo-link-context' //Setear cabezeras en el request
+import { InMemoryCache } from 'apollo-cache-inmemory' //Cache graphl cache
+
+const API_URL = "aws";
+
+const httplink = createHttpLink({
+    uri:`${API_URL}/graphql`,
+    //credentials: "include" //solo se agregan cuando hay credenciales en el backend
+});
+
+const authLink = setContext((_,{headers})=>{
+    const token = localStorage.getItem('postsToken');
+    return {
+        headers:{
+            ...headers,
+
+            authorization: token ? `JWT ${token}` : ''
+        }
+    }
+});
+
+export default new ApolloClient({
+    link: authLink.concat(httplink),
+    cache: new InMemoryCache()
+})
